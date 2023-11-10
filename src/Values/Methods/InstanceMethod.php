6deecf178d55ba91bd96b899f135fc7f3294cd76
ascetik <ascetik@free.fr>
2023-11-10 19:59:49 +0100

    doc

--- src/Values/Methods/InstanceMethod.php
+++ src/Values/Methods/InstanceMethod.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This is part of the ascetik/callabubble package
+ *
+ * @package    Callabubble
+ * @category   Value Object
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\Callabubble\Values\Methods;
 
 use Ascetik\Callabubble\Types\ClassMethod;
@@ -7,8 +19,8 @@ use Ascetik\Callabubble\Types\ClassMethod;
 class InstanceMethod implements ClassMethod
 {
     public function __construct(
-        public readonly object $instance,
-        public readonly string $method
+        private object $instance,
+        private string $method
     ) {
     }
 
--- src/Values/Methods/StaticMethod.php
+++ src/Values/Methods/StaticMethod.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This is part of the ascetik/callabubble package
+ *
+ * @package    Callabubble
+ * @category   Value Object
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\Callabubble\Values\Methods;
 
 class StaticMethod
