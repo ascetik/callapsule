d2d0b64dd27f222b4123e6c090eff3b20cfaa5bc
ascetik <ascetik@free.fr>
2023-11-10 17:44:56 +0100

    3 new Exceptions thrown by static factories

--- src/Exceptions/ClassNotFoundException.php
+++ src/Exceptions/ClassNotFoundException.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This is part of the ascetik/callabubble package
+ *
+ * @package    Callabubble
+ * @category   Exception
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\Callabubble\Exceptions;
 
 class ClassNotFoundException extends \InvalidArgumentException
