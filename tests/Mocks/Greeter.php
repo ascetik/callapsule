c43909c4e3dd66229d82680af0eeb0194c580874
ascetik <ascetik@free.fr>
2023-11-10 17:05:51 +0100

    new methods for Greeter mock

--- tests/Mocks/Greeter.php
+++ tests/Mocks/Greeter.php
@@ -4,8 +4,18 @@ namespace Ascetik\Callabubble\Tests\Mocks;
 
 class Greeter
 {
+    public function __invoke(string $name)
+    {
+        return 'hi ' . $name;
+    }
+    
     public function greet(string $name)
     {
-        return 'hello '.$name;
+        return 'hello ' . $name;
+    }
+
+    public static function bye(string $name)
+    {
+        return 'bye ' . $name;
     }
 }
